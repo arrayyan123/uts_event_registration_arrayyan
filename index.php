7653abba89aefc872da765bc005c8a58dca13606
arrayyan aprilyanto <146199861+arrayyan123@users.noreply.github.com>
2024-11-07 21:20:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
 
-    $requestUri = isset($_SERVER['REQUEST_URI']) ? filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL) : '/';
+$requestUri = isset($_SERVER['REQUEST_URI']) ? filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL) : '/';
 
-    if ($requestUri === '/' || $requestUri === '') {
-        include 'src/views/home.php';
-        exit();
-    }
-
-    $requestUri = trim($requestUri, '/');
-
-    if (strpos($requestUri, '..') !== false) {
-        include 'src/views/404.php';
-        exit();
-    }
-
-    if (file_exists("src/views/$requestUri.php")) {
-        include "src/views/$requestUri.php";
-    } else {
-        include 'src/views/404.php';
-    }
-
+if ($requestUri === '/' || $requestUri === '') {
+    include 'src/views/home.php';
     exit();
+}
+
+$requestUri = trim($requestUri, '/');
+
+if (strpos($requestUri, '..') !== false) {
+    include 'src/views/home.php';
+    exit();
+}
+
+if (file_exists("src/views/$requestUri.php")) {
+    include "src/views/$requestUri.php";
+} else {
+    include 'src/views/home.php';
+}
+
+exit();
 ?>
